f555b5abd7a181693334fb8dd14004488b60e3b3
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-02 11:48:24 +0100

    changes

--- Arnaldo/Basic/scriptbasic.js
+++ Arnaldo/Basic/scriptbasic.js
@@ -3,3 +3,12 @@
 
 let temp = Math.floor(Math.random() * 11);
 console.log(temp);
+if (temp < -10) {
+    console.log("Very Cold");
+} else if (temp < 10) {
+    console.log("Cold");
+} else if (temp < 20) {
+    console.log("ok");
+} else {
+    console.log("Hot");
+}
