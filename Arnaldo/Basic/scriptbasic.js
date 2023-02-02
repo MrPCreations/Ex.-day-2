c40d912f5c5abafc8ed1ca81032561f7e32c6eb7
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-02 14:14:05 +0100

    changes

--- Arnaldo/Basic/scriptbasic.js
+++ Arnaldo/Basic/scriptbasic.js
@@ -15,3 +15,10 @@ let food = ["pizza", "hamburger", "ice cream", "chocolate"];
 console.log("The Meal of the Day is " + food[random]);
 
 // ex 3 | Crystal Gazer
+
+function crystalGlazer(job, Location, partners_Name, Children) {
+    let infos = `You will be a ${job}, in ${Location}, and married to ${partners_Name}, with ${Children}.`;
+    console.log(infos)
+}
+crystalGlazer("dentist", "Italy", "Julia", 4);
+crystalGlazer("doctor", "Malta", "Joanna", 2)
